5f0e9ead4307947f7836f7e570173c205a672c93
JerryOrange <563146556@qq.com>
2018-06-28 20:11:59 +0800

    synchronized 同步方法块

---
+++ concurrent/SynchronizedBlock.java
@@ -0,0 +1,39 @@
+/**
+ * 同步方法块
+ *
+ * Created by Jiacheng on 2018/6/28.
+ */
+public class SynchronizedBlock {
+
+    public void method1() {
+        System.out.println("Method 1 start");
+        try {
+            synchronized (this) {
+                System.out.println("Method 1 execute");
+                Thread.sleep(3000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Method 1 end");
+    }
+
+    public void method2() {
+        System.out.println("Method 2 start");
+        try {
+            synchronized (this) {
+                System.out.println("Method 2 execute");
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Method 2 end");
+    }
+
+    public static void main(String[] args) {
+        final SynchronizedBlock test = new SynchronizedBlock();
+        new Thread(() -> test.method1()).start();
+        new Thread(() -> test.method2()).start();
+    }
+}
